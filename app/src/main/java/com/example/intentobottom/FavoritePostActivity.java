4d3ad49d58a92ced6a19e5dea4f05f13d989a7d6
Jorge Kawamura Solis <jorge.kawamurasolis@gmail.com>
2022-05-03 16:22:46 -0500

    Feature: Favourite posts layout with cards

---
+++ app/src/main/java/com/example/intentobottom/FavoritePostActivity.java
@@ -0,0 +1,14 @@
+package com.example.intentobottom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FavoritePostActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favorite_post);
+    }
+}
